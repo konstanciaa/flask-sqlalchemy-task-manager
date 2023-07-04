97ac7daabe1934b8886cf095dbf7b74391fb9d70
Anna Konstantynova <hannakonstantinova@gmail.com>
2023-07-04 18:27:15 +0000

    Add basic html structure and styles

--- taskmanager/models.py
+++ taskmanager/models.py
@@ -15,11 +15,11 @@ class Category(db.Model):
 class Task(db.Model):
     # schema for the Task model
     id = db.Column(db.Integer, primary_key=True)
-    task_name = db.column(db.String(50), unique=True, nullable=False)
+    task_name = db.Column(db.String(50), unique=True, nullable=False)
     task_description = db.Column(db.Text, nullable=False)
     is_urgent = db.Column(db.Boolean, default=False, nullable=False)
     due_date = db.Column(db.Date, nullable=False)
-    category_id = db.Column(db.Integer, db.ForeignKey("category_id", ondelete="CASCADE"), nullable=False)
+    category_id = db.Column(db.Integer, db.ForeignKey("category.id", ondelete="CASCADE"), nullable=False)
 
     def __repr__(self):
         # __repr__ to represent itself in the form of a string
